82f862ef5f928f655c727e5f492dbbd4e4061240
Jack Hedaya <jackehedaya@gmail.com>
2019-04-10 22:57:47 -0400

    added compilation unit tests

--- tests/compiler.test.ts
+++ tests/compiler.test.ts
@@ -1,11 +1,32 @@
 import Compiler from "../src/compiler";
 
-const equation = "2x + 5 = 20";
+var c = new Compiler();
+
+enum CharacterType {
+  EXPRESSION, WHITESPACE, OPERATOR, COMPARISON, ERROR
+}
 
 describe("Compiler", () => {
-  test("should fail with weird input", () => {
-    var c = new Compiler();
-    
-    expect(c.compile("@#@nd")).toThrow();
+  test("should throw with start to equation", () => {
+    expect(() => c.compile("@#@nd")).toThrow();
+  });
+
+  test("should not throw with normal start to equation", () => {
+    expect.assertions(3);
+
+    expect(() => c.compile("2x + 3 = 3")).not.toThrow();
+    expect(() => c.compile("xy + 2 = 1")).not.toThrow();
+    expect(() => c.compile("(3x + 4) = 3")).not.toThrow();
+  });
+});
+
+describe("Compiler#characterToType", () => {
+  test("should return the correct type", () => {
+    expect.assertions(4);
+
+    expect(c.characterToType("+")).toBe(CharacterType.OPERATOR);
+    expect(c.characterToType("2")).toBe(CharacterType.EXPRESSION);
+    expect(c.characterToType("(")).toBe(CharacterType.EXPRESSION);
+    expect(c.characterToType("x")).toBe(CharacterType.EXPRESSION);
   });
 });
