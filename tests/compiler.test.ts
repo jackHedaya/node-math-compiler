382025ea2750082019877f610e1b1c29780acfcc
Jack Hedaya <jackehedaya@gmail.com>
2019-04-11 21:44:43 -0400

    added precompile unit tests

--- tests/compiler.test.ts
+++ tests/compiler.test.ts
@@ -17,6 +17,32 @@ describe("Compiler", () => {
   });
 });
 
+describe("Compiler#precompile", () => {
+  test("should crash on invalid equation", () => {
+    expect(() => c["precompile"]("(( x + 2) = 3")).toThrow();
+  });
+
+  test("should not crash on valid equation", () => {
+    expect(() => c["precompile"]("(3x) - 4 = (2/3+1)")).not.toThrow();
+  });
+
+  test("should not crash on equation that contains no parenthesis", () => {
+    expect(() => c["precompile"]("2x + 4 = 6")).not.toThrow();
+  });
+
+  test("should crash on equation that contains only '('", () => {
+    expect(() => c["precompile"]("(X + 3 = 0")).toThrow()
+  });
+  
+  test("should crash on equation that contains only ')'", () => {
+    expect(() => c["precompile"](")3 + 4 = 3X")).toThrow()
+  });
+
+  test("should add 0 to equation that starts with '-'", () => {
+    expect(c["precompile"]("-24x = 5")).toBe("0-24x = 5");
+  });
+});
+
 describe("Compiler#characterToType", () => {
   test("should return the correct type", () => {
     expect.assertions(4);
