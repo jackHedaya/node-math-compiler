263859ae206c94de019dddbdc4cf098a432a28b4
Jack Hedaya <jackehedaya@gmail.com>
2019-04-11 21:00:46 -0400

    add unit tests for generateLogic

---
+++ tests/token.test.ts
@@ -0,0 +1,29 @@
+import Token, { TokenType } from "../src/token";
+import { CharacterType } from "../src/compiler";
+
+describe("Token#generateLogic", () => {
+  test("should return the correct token", () => {
+    expect.assertions(4);
+
+    expect(
+      Token.generateLogic(CharacterType.EXPRESSION, {
+        [CharacterType.EXPRESSION]: TokenType.EXPRESSION_START,
+      }),
+    ).toBe(TokenType.EXPRESSION_START);
+    expect(
+      Token.generateLogic(CharacterType.EXPRESSION, {
+        [CharacterType.OPERATOR]: TokenType.COMPILER_START,
+      }),
+    ).toBe(TokenType.ERROR);
+    expect(
+      Token.generateLogic(CharacterType.EXPRESSION, {
+        [CharacterType.EXPRESSION]: TokenType.EXPRESSION_CONTINUE,
+      }),
+    ).toBe(TokenType.EXPRESSION_CONTINUE);
+    expect(
+      Token.generateLogic(CharacterType.WHITESPACE, {
+        [CharacterType.WHITESPACE]: TokenType.COMPILER_START
+      }),
+    ).toBe(TokenType.COMPILER_START);
+  });
+});
